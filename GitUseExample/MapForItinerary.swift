38d7e1390add4deb6e80099663e1d0511cb59cc7
Steven Roberts <slro253@g.uky.edu>
2019-09-13 12:40:55 -0400

    Feature branch added

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Steven Roberts on 9/13/19.
+//  Copyright © 2019 StevenPortfolio. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
